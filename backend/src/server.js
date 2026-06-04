2b0532859be93d9a32276ac6a4f0910e2156d5cc
AaravAnand31 <aaravanandbbps@gmail.com>
2026-06-04 15:07:52 +0530

    Updated project

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,8 +7,8 @@ const cors       = require("cors");
 console.log("Starting server...");
 
 const authRoutes       = require("./routes/authRoutes");
-const userRoutes       = require("./routes/userRoutes");        // NEW
-const connectionRoutes = require("./routes/connectionRoutes");  // NEW
+const userRoutes       = require("./routes/userRoutes");
+const connectionRoutes = require("./routes/connectionRoutes");
 
 const app = express();
 
@@ -16,8 +16,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth",        authRoutes);
-app.use("/api/users",       userRoutes);        // NEW
-app.use("/api/connections", connectionRoutes);  // NEW
+app.use("/api/users",       userRoutes);
+app.use("/api/connections", connectionRoutes);
 
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
